594c38feffa655720374e28025125ac3e80e21df
Shang Sinian <shangsinian@gmail.com>
2016-05-31 02:17:02 +0800

    no message

--- thriftService.js
+++ thriftService.js
@@ -2,7 +2,7 @@ var thrift = require('thrift');
 
 var Cal = require('./thrift/gen-nodejs/CalService');
 
-var Conn = thrift.createConnection("test-openshift-be.ghuang.local", 10000);
+var Conn = thrift.createConnection("172.30.154.130", 10000);
 
 Conn.on('error', function(err) {
   console.log(err)
